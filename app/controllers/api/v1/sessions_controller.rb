9a32a015600b2b418defc98b0637f766ba25ac0c
“qwbtc” <bob.dylan0313@gmail.com>
2017-05-31 03:52:15 +0800

    cv

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,15 @@ class Api::V1::SessionsController < Api::ApplicationController
     @user = User.find_by(email: create_params[:email])
     if @user && @user.authenticate(create_params[:password])
       self.current_user = @user
-      render json: @user
+      user = {
+        data: {
+          user: @user,
+          token: @user.authentication_token,
+          email: @user.email
+        }
+      }
+      render json: user
+      # render json: @user
     else
       render json: error(401, '验证失败')
     end
