de01cc50b38218797d6073ed5e3bc03f99fab189
“qwbtc” <bob.dylan0313@gmail.com>
2017-06-01 22:43:59 +0800

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,54 @@
-require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'
-#require 'mina/rvm'
-require 'mina/puma'
-require 'mina/logs'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
-set :domain, '106.15.92.227'
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :application_name, 'isaac_api'
 set :user, 'qwbtc'
+set :domain, '106.15.92.227'
 set :deploy_to, '/home/qwbtc/var/www/isaac_api'
-set :repository, 'git@github.com:qwbtc/blog_api.git'    # private repository
+set :repository, 'git@github.com:qwbtc/blog_api.git'
 set :branch, 'master'
 
-set :shared_dirs, fetch(:shared_dirs, []).push('public/upload', 'tmp/sockets', 'tmp/pids', 'db/production')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+# Optional settings:
+#   set :user, 'foobar'          # Username in the server to SSH to.
+#   set :port, '30000'           # SSH port number.
+#   set :forward_agent, true     # SSH forward_agent.
 
-desc 'Set up environment.'
+# shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
 task :environment do
-  invoke :'rbenv:load'
-  #invoke :'rvm:use', 'ruby-2.3.1@rails5'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
 end
 
-desc 'Prepare for deployment.'
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
-  in_path "#{fetch(:shared_path)}" do
-    ['config/database.yml', 'config/secrets.yml'].each do |file|
-      comment %{Be sure to edit 'shared/#{file}'.}
-    end
-  end
+  # command %{rbenv install 2.3.0}
 end
 
-desc 'Deploy current version to the server.'
-task :deploy => :environment do
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
   deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -41,8 +57,17 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     on :launch do
-      invoke :'puma:start'
-      invoke :'puma:phased_restart'
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
     end
   end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
 end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - https://github.com/mina-deploy/mina/tree/master/docs
