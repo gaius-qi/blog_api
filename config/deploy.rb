d3b43a44767f6c6ad1689a7c65f9e506e19c651a
“qwbtc” <bob.dylan0313@gmail.com>
2017-06-01 23:04:07 +0800

    database

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
 require 'mina/rails'
 require 'mina/git'
-require 'mina/puma'
-require 'mina/logs'
-
 # require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
@@ -25,8 +22,9 @@ set :branch, 'master'
 #   set :forward_agent, true     # SSH forward_agent.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
@@ -42,16 +40,15 @@ end
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
   # command %{rbenv install 2.3.0}
-  in_path "#{fetch(:shared_path)}" do
-    ['config/database.yml', 'config/secrets.yml'].each do |file|
-      comment %{Be sure to edit 'shared/#{file}'.}
-    end
-  end
 end
 
-desc 'Deploy current version to the server.'
-task :deploy => :environment do
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
   deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -60,14 +57,16 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     on :launch do
-      invoke :'puma:start'
-      invoke :'puma:phased_restart'
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
     end
   end
-end
+
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
   # run(:local){ say 'done' }
-
+end
 
 # For help in making your deploy script, see the Mina documentation:
 #
