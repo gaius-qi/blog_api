b55660ef03af5981a81d66d534db593100e45d86
“qwbtc” <bob.dylan0313@gmail.com>
2017-06-03 17:46:34 +0800

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,7 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 #在本目录下生成pid方便kill
-pidfile "./puma.pid"
+pidfile "../puma.pid"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
